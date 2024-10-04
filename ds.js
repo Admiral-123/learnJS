1fb503a87b70a258bb78a3c9013e68c1a3ce98d2
admiral <kartikeyk1776@gmail.com>
2024-10-04 16:35:08 +0530

    data type added

---
+++ ds.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as newer version of js\
+
+
+// alert(4+8) // it works in browser not in node
+
+
+let x = "hlo"
+let len = 3
+let isStr = true
+
+
+/* 
+number range= 2^53
+bigint - for very large no.
+boolean => true/false
+null -> in js it is standalone value
+undefined -> var declared but not assigned
+symbol -> for unique
+
+
+object
+
+*/
+
+console.table([typeof isStr, typeof x, typeof len])
+
+console.log(typeof null)        // object
+console.log(typeof undefined)   // undefined
