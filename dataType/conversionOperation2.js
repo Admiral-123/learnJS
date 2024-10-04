8116b4decd9ff80b2670ba8e1ec402b57155796a
admiral <kartikeyk1776@gmail.com>
2024-10-04 18:01:29 +0530

    dataType dir added

---
+++ dataType/conversionOperation2.js
@@ -0,0 +1,31 @@
+let y=5
+let minusY = -y;
+// console.log(minusY)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2
+
+// let str1= "hello"
+// let str2= " steve"
+// let str3=str1+str2
+
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(1+"2"+1)
+
+
+let x = 4
+let z = ++x
+
+console.log(`x:${x}, z:${z}`);      // the value of x is incremented first then assigned to z
+
+let e = 7
+let f = e++
+console.log(`e:${e}, f:${f}`)       // val of e is assigned to f first then incremented
