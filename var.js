e609b64fdf06d8dcd3dc73f1820f5a88014e2eaf
admiral <kartikeyk1776@gmail.com>
2024-10-04 15:21:49 +0530

    add var.js

---
+++ var.js
@@ -0,0 +1,18 @@
+const name="ben"    // cant change const var
+
+let strikeRate= 96
+
+var jersey = "54"
+
+
+let wickets;        // it'll be undefined
+
+strikeRate=95
+
+jersey = "55"
+
+console.log(name)
+
+console.table([strikeRate, jersey, wickets])
+
+// in javascript only use const or let because of issue in block scope and functional scope
