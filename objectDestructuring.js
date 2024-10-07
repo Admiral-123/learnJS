6b7a770f735e32916ecffb6b0ad72e82d1a804f8
admiral <kartikeyk1776@gmail.com>
2024-10-07 20:19:36 +0530

    added obj destructuring and json api intro

---
+++ objectDestructuring.js
@@ -0,0 +1,32 @@
+const h = {
+    name: "ronaldo",
+    age:36,
+    goalInternational: 132
+}
+
+const {age} = h     // known as destructuring; taking a prop. of object 
+
+const {goalInternational: gI}= h        // can also destructure the prop. and assign it to another var (like gI in this case)
+
+console.log(age)
+
+console.log(gI)
+
+
+// api:
+
+// in json :-
+
+// {
+//     name:""
+//     email:""
+//     gender:""
+// }
+
+// in array :-
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
