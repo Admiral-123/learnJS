b16b0dcfafd7fa0aaf34209a4ffdf1c44d7b2022
admiral <kartikeyk1776@gmail.com>
2024-10-10 15:08:33 +0530

    about do while and while loops

---
+++ iterations2.js
@@ -0,0 +1,30 @@
+
+
+let index = 0 
+
+while (index <=9){
+    console.log(`val of index is ${index}`)
+    index++
+}
+
+let myArr = ['mic', 'hlo', 'soup bois']
+
+let arr = 0;
+
+while (arr < myArr.length) {
+    const element = myArr[arr]
+    console.log(element)
+    arr++;
+}
+
+
+
+
+// do while loop - keep doing it till a condition is valid
+let s1 = 1
+
+do {
+    let output = s1++
+    console.log(output)
+}
+while(s1 < 9)
