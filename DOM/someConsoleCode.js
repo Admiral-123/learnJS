da6d4f9a64e4be48013cfa502f9becf53124afb5
admiral <kartikeyk1776@gmail.com>
2024-10-11 15:52:42 +0530

    about DOM

---
+++ DOM/someConsoleCode.js
@@ -0,0 +1,9 @@
+console.log(window) // info about window of the tab
+
+console.log(window.document)
+
+console.log(document)
+
+console.dir(document)
+
+console.log(document.links)     // returns links in the document in HTMLcollection
